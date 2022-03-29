eca674497f0d0855ba244e6f2f58bf76f2ab5ba8
nakfl <nguyenhuugiatri@gmail.com>
2022-03-29 18:16:32 +0700

    chore: update docs

--- demo/src/pages/index/index.js
+++ demo/src/pages/index/index.js
@@ -3,32 +3,32 @@ Page({
     chartData: [
       {
         date: '2020/02/01',
-        value: 0,
+        value: 0
       },
       {
         date: '2020/02/02',
-        value: 900,
+        value: 900
       },
       {
         date: '2020/02/03',
-        value: 2300,
+        value: 2300
       },
       {
         date: '2020/02/04',
-        value: 800,
+        value: 800
       },
       {
         date: '2020/02/05',
-        value: 1600,
+        value: 1600
       },
       {
         date: '2020/02/06',
-        value: 2900,
+        value: 2900
       },
       {
         date: '2020/02/07',
-        value: 3800,
-      },
-    ],
-  },
+        value: 3800
+      }
+    ]
+  }
 });
